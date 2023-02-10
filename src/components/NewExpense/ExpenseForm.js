7bcab369ed4d319d412b8591c613d6b75abd8133
jhaganb <jhaganb@gmail.com>
2023-02-10 20:24:33 +0530

    made a 2 way binding for form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -29,12 +29,31 @@ const ExpenseForm = () => {
     // setUserInput({ ...userInput, enteredDates: event.target.value });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -42,6 +61,7 @@ const ExpenseForm = () => {
             type="number"
             min="0.01"
             step="0.01"
+            value={setEnteredAmount}
             onClick={amountChangeHandler}
           />
         </div>
@@ -51,6 +71,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2023-12-31"
+            value={setEnteredDate}
             onChange={dateChangeHandler}
           />
         </div>
